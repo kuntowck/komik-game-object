47d18869d661534933d277be7cc4df82a88ab664
kuntowck <kunto312002@gmail.com>
2024-03-23 22:19:43 +0700

    constructor

--- index.php
+++ index.php
@@ -3,9 +3,18 @@
 class Produk
 {
   public $judul,
-        $penulis,
-        $penerbit,
-        $harga;
+    $penulis,
+    $penerbit,
+    $harga;
+
+  // constructor merupakan method yang otomatis dijalankan ketika instance object.
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = "harga")
+  {
+    $this->judul = $judul;
+    $this->penulis = $penulis;
+    $this->penerbit = $penerbit;
+    $this->harga = $harga;
+  }
 
   public function getLabel()
   {
@@ -15,17 +24,8 @@ class Produk
 
 
 // instance object Produk
-$produk1 = new Produk();
-$produk1->judul = "Naruto";
-$produk1->penulis = "Masashi Kishimoto";
-$produk1->penerbit = "Shonen Jump";
-$produk1->harga = 30000;
-
-$produk2 = new Produk();
-$produk2->judul = "Red Dead Redemption 2";
-$produk2->penulis = "Dan Houser";
-$produk2->penerbit = "Rockstar Games";
-$produk2->harga = 650000;
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
+$produk2 = new Produk("Red Dead Redemption 2", "Dan Houser", "Rockstar Games", 650000);
 
 echo "Komik: " . $produk1->getLabel();
 echo "<br><br>";
