220a2f26b667b554b68606b92b59271188379ba4
kuntowck <kunto312002@gmail.com>
2024-03-23 22:36:37 +0700

    object type

--- index.php
+++ index.php
@@ -18,15 +18,29 @@ class Produk
 
   public function getLabel()
   {
-    return "$this->judul, $this->penulis, $this->penerbit, $this->harga";
+    return "$this->judul, $this->penulis, $this->penerbit";
   }
 }
 
+class CetakInfoProduk
+{
+  // object type untuk menspesifikasi parameter menggunakan object sebagai tipe data
+  public function cetak(Produk $produk)
+  {
+    $str = "{$produk->judul} | {$produk->getLabel()} (Rp{$produk->harga})";
+    return $str;
+  }
+}
 
 // instance object Produk
 $produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
 $produk2 = new Produk("Red Dead Redemption 2", "Dan Houser", "Rockstar Games", 650000);
 
+$infoProduk1 = new CetakInfoProduk();
+
 echo "Komik: " . $produk1->getLabel();
 echo "<br><br>";
 echo "Games: " . $produk2->getLabel();
+echo "<br><br>";
+echo $infoProduk1->cetak($produk1);
+
