f209b8506ae058397d659d86f28ae4f36df1a504
kuntowck <kunto312002@gmail.com>
2024-03-26 11:51:46 +0700

    menambahkan property dan method baru pada class produk

--- index.php
+++ index.php
@@ -5,20 +5,38 @@ class Produk
   public $judul,
     $penulis,
     $penerbit,
-    $harga;
+    $harga,
+    $jmlHalaman,
+    $durasi,
+    $tipe;
 
-  // constructor merupakan method yang otomatis dijalankan ketika instance object.
-  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = "harga")
+  // constructor, method yang otomatis dijalankan ketika instance object.
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = "harga", $jmlHalaman = 0, $durasi = 0, $tipe)
   {
     $this->judul = $judul;
     $this->penulis = $penulis;
     $this->penerbit = $penerbit;
     $this->harga = $harga;
+    $this->jmlHalaman = $jmlHalaman;
+    $this->durasi = $durasi;
+    $this->tipe = $tipe;
   }
 
   public function getLabel()
   {
-    return "$this->judul, $this->penulis, $this->penerbit";
+    return "$this->penulis, $this->penerbit";
+  }
+
+  public function getInfo()
+  {
+    $str = "{$this->tipe}: {$this->judul} - {$this->getLabel()} | Rp{$this->harga}";
+    if ($this->tipe == 'komik') {
+      $str .= " - {$this->jmlHalaman} halaman.";
+    } elseif ($this->tipe == 'game') {
+      $str .= " ~ {$this->durasi} jam.";
+    }
+
+    return $str;
   }
 }
 
@@ -27,20 +45,15 @@ class CetakInfoProduk
   // object type untuk menspesifikasi parameter menggunakan object sebagai tipe data
   public function cetak(Produk $produk)
   {
-    $str = "{$produk->judul} | {$produk->getLabel()} (Rp{$produk->harga})";
+    $str = "{$produk->judul} - {$produk->getLabel()} | Rp{$produk->harga}";
     return $str;
   }
 }
 
 // instance object Produk
-$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
-$produk2 = new Produk("Red Dead Redemption 2", "Dan Houser", "Rockstar Games", 650000);
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0, "komik");
+$produk2 = new Produk("Red Dead Redemption 2", "Dan Houser", "Rockstar Games", 650000, 0, 50, "game");
 
-$infoProduk1 = new CetakInfoProduk();
-
-echo "Komik: " . $produk1->getLabel();
-echo "<br><br>";
-echo "Games: " . $produk2->getLabel();
-echo "<br><br>";
-echo $infoProduk1->cetak($produk1);
-
+echo $produk1->getInfo();
+echo "<br>";
+echo $produk2->getInfo();
