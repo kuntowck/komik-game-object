342474bee1eb1fdc1a1dca063c770b9dd11d3642
kuntowck <kunto312002@gmail.com>
2024-04-17 19:55:52 +0700

    autoload debug

--- App/init.php
+++ App/init.php
@@ -1,29 +1,12 @@
 <?php
 
-// require_once 'Produk/InfoProduk.php';
-// require_once 'Produk/Produk.php';
-// require_once 'Produk/Komik.php';
-// require_once 'Produk/Game.php';
-// require_once 'Produk/CetakInfoProduk.php';
+require_once 'Produk/InfoProduk.php';
+require_once 'Produk/Produk.php';
+require_once 'Produk/Komik.php';
+require_once 'Produk/Game.php';
+require_once 'Produk/CetakInfoProduk.php';
 
 
 spl_autoload_register(function ($class) {
-   // $path = str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
-
-   // if (file_exists($path)){
-   //    require $path;
-
-   //    return true;
-   // }
    require_once 'Produk/' . $class . '.php';
 });
-
-
-// function loadModel($class)
-// {
-//    $path = 'Produk/' . $class . '.php';
-
-//    if (file_exists($path)) require $path;
-// }
-
-// spl_autoload_register('loadModel');
